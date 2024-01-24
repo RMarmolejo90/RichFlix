1bc92de4adf7f963fcaff9650d6c4bcfe7e717ec
Richard Marmolejo <109112094+RMarmolejo90@users.noreply.github.com>
2024-01-24 13:15:28 +0000

    started routing and structure

---
+++ richflix/src/app/[movie_details]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div> 
+        {/* image */}
+        {/* cast */}
+        {/* description */}
+    </div>
+  )
+}
---
+++ richflix/src/app/search/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function search() {
+  return (
+    // search logic
+
+    // handleSubmit
+
+
+    <div>
+        {/* main nav */}
+        {/* search bar */}
+        {/* suspense field / display results */}
+    </div>
+  )
+}
