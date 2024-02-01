05a9352d9c8e4e9909c39eaf9fb8d7f9b6c61428
Richard Marmolejo <109112094+RMarmolejo90@users.noreply.github.com>
2024-02-01 12:58:09 +0000

    fetch images in movieList component

--- richflix/src/components/movieList.tsx
+++ richflix/src/components/movieList.tsx
@@ -1,13 +1,23 @@
 import { type } from 'os';
 import React, { useEffect, useState } from 'react'
 import requests from '@/lib/movies/requests';
+import Image from 'next/image';
 
 // MovieList component displays the rows of fetched movie data
 
+
 const MovieCard: React.FC<Movie> = (movie) => {
+  const imageSize = "w342";
+  const posterUrl = `http://image.tmdb.org/t/p/${imageSize}${movie.poster_path}`;
   return (
-    <div>
-      <image href={movie.poster_path} />
+    <div className='flex flex-row'>
+      <Image 
+        src={posterUrl}
+        alt={movie.title}
+        width={100} 
+        height={100}
+        quality={100}
+      />
       <h3>{movie.title}</h3>
     </div>
   )
@@ -29,7 +39,7 @@ const MovieList = async ({fetchUrl}: Fetchurl) => {
   const movies = await getMovies(fetchUrl)
 
   return (
-    <div>
+    <div className='flex flex-row'>
       {movies.map((movie:Movie) => (<MovieCard key={movie.id} {...movie}/>))}        
     </div>
   );
