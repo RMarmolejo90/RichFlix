33a3514df40842dfcc56481d37e6ced97eb50889
Richard Marmolejo <109112094+RMarmolejo90@users.noreply.github.com>
2024-02-16 22:03:01 +0000

    unfinished change of the header component

--- richflix/src/components/header.tsx
+++ richflix/src/components/header.tsx
@@ -1,41 +1,18 @@
-"use client"
-
 import Image from 'next/image'
 import requests from '@/lib/movies/requests';
-import { useEffect, useState } from 'react';
+import fetchRandomMovie from '@/utils/fetchRandomMovie.ts'
 
-const headerMoviesURL: string = requests.nowPlaying;
+// this is the url to select a random movie from
+const movieURL: string = requests.nowPlaying;
+
+const displayMovie = await fetchRandomMovie(movieURL);
 
 const header = () => {
   
-  const [movies, setMovies] = useState<Movie[]>();
-  const [displayMovie, setDisplayMovie] = useState<Movie>();
   const backdropPath: string | null = displayMovie?.backdrop_path ?? null;
   const backdrop: string = `https://image.tmdb.org/t/p/w1280${backdropPath}`;
   const altTag: string = displayMovie?.title??"Movie Backdrop image";
   
-  useEffect(()=> {
-    async function getMovies(fetchUrl: string): Promise<Movie[]> {
-      console.log('fetching data');
-      const response = await fetch(fetchUrl, requests.options);
-      const data: QueryData = await response.json();
-      if (!data) {throw Error(`error fetching data from ${fetchUrl}`)}
-      else {
-        const movies = data.results;
-        console.log(movies);
-        setMovies(movies);
-        return movies
-      }
-    } getMovies(headerMoviesURL);
-  }, [])
-  
-  useEffect(() => {
-    if (movies)
-      {const randomMovie = movies[Math.floor(Math.random() * movies.length)];
-      setDisplayMovie(randomMovie);
-      console.log("setDisplay");}
-  }, [movies])
-
 
   return (
     <div className='w-full h-[500px] top-0 text-white overflow-hidden relative '>
