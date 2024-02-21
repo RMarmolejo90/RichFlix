fa9b40bdbf3392d8c5e93be9dc1224e0e53f4adf
Richard Marmolejo <109112094+RMarmolejo90@users.noreply.github.com>
2024-02-21 02:08:22 +0000

    add image to details page

--- richflix/src/app/movies/[movieId]/page.tsx
+++ richflix/src/app/movies/[movieId]/page.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import fetchMovieDetails from '@/app/_utils/fetchMovieDetails';
+import Image from 'next/image';
+
 
 type Params = {
   params: {
@@ -9,19 +11,29 @@ type Params = {
 
 export default async function page({ params: { movieId } }: Params) {
 
-  const movie = await fetchMovieDetails(movieId);
-  
+    const movie = await fetchMovieDetails(movieId);
+    const posterSize: string =  "w780"; 
+    const altTag: string =  movie.title ? movie.title : "Movie Poster Image";
   // fetch movie === movie.id from movieDB API
 
   // data === response.data
   // return data - destructure {image, cast, description}
+  const posterUrl = `http://image.tmdb.org/t/p/${posterSize}${movie.poster_path}`;
 
   return (
-    <div> 
-      <h1>{movie.title}</h1>
-        {/* image */}
+    <>
+    <div className='flex flex-col items-center mt-14'> 
+      <Image 
+        src={posterUrl}
+        alt={altTag}
+        width={200} 
+        height={200}
+        quality={100} 
+      />
+      <h1 className='m-4 text-3xl'>{movie.title}</h1>
+    </div>
         {/* cast */}
         {/* description */}
-    </div>
+  </>
   )
 }
