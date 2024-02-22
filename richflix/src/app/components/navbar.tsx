7cba8945a07c5857bbace7debd0ebfd6ea31cfcd
Richard Marmolejo <109112094+RMarmolejo90@users.noreply.github.com>
2024-02-22 02:03:23 +0000

    make navbar logo a link to home

--- richflix/src/app/components/navbar.tsx
+++ richflix/src/app/components/navbar.tsx
@@ -16,29 +16,31 @@ export default function Navbar() {
   const { userId }: {userId: string | null} = auth();
   return (
     <div className='flex flex-row w-full align-top content-between p-2 z-50 bg-gradient-to-b from-[#09090bCC] absolute top-0'>
+      <Link href="/">
         <h1 className={`${bebas.className} mr-auto text-red-600 p-2 text-4xl tracking-wider`}>
             RICHFLIX
         </h1>
-        <div className='ml-auto lg:my-8 font-semibold rounded-md tracking-widest text-lg'>
-          
-          {// if not logged in, display signin/signup buttons
-          !userId ?
-            <div className='flex flex-row'>
-              <div className='m-2 hover:border-b-2 hover:cursor-pointer'>
-                <SignInButton afterSignInUrl='/'/>
-              </div>
-              <div className='m-2 hover:border-b-2 hover:cursor-pointer'>
-                <SignUpButton />
-              </div>
+      </Link>    
+      <div className='ml-auto lg:my-8 font-semibold rounded-md tracking-widest text-lg'>
+        
+        {// if not logged in, display signin/signup buttons
+        !userId ?
+          <div className='flex flex-row'>
+            <div className='m-2 hover:border-b-2 hover:cursor-pointer'>
+              <SignInButton afterSignInUrl='/'/>
             </div>
-              :   // if logged in, display user button       
-            <div>
-              <UserButton 
-                afterSignOutUrl="/"
-              />
-            </div> 
-          }
-        </div>
+            <div className='m-2 hover:border-b-2 hover:cursor-pointer'>
+              <SignUpButton />
+            </div>
+          </div>
+            :   // if logged in, display user button       
+          <div>
+            <UserButton 
+              afterSignOutUrl="/"
+            />
+          </div> 
+        }
+      </div>
     </div>
   )
 }
