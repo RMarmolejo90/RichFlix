55d6b0527f1f002a5df9d55eef9bd7fe0a66f2a2
Richard Marmolejo <109112094+RMarmolejo90@users.noreply.github.com>
2024-02-22 01:59:51 +0000

    add genres to details page

--- richflix/src/app/movies/[movieId]/page.tsx
+++ richflix/src/app/movies/[movieId]/page.tsx
@@ -15,27 +15,36 @@ export default async function page({ params: { movieId } }: Params) {
     const posterSize: string =  "w780"; 
     const altTag: string =  movie.title ? movie.title : "Movie Poster Image";
     const description = movie?.overview??"";
+    const genres = Object.values(movie.genres);
+    const genresName = genres.map(genre => genre.name);
 
   // fetch movie === movie.id from movieDB API
 
   // data === response.data
   // return data - destructure {image, cast, description}
   const posterUrl = `http://image.tmdb.org/t/p/${posterSize}${movie.poster_path}`;
-
-  return (
-    <>
-    <div className='flex flex-col items-center mt-14'> 
-      <Image 
-        src={posterUrl}
-        alt={altTag}
-        width={200} 
-        height={200}
-        quality={100} 
-      />
-      <h1 className='m-4 text-3xl'>{movie.title}</h1>
+  console.log(genres);
+  console.log(genresName);
+    return (
+    <div className='flex flex-col justify-center items-center'>
+      <div className='flex flex-row items-between justify-center mt-14'>
+        <div className='w-1/4 m-2'> 
+          <Image 
+            src={posterUrl}
+            alt={altTag}
+            width={200} 
+            height={200}
+            quality={100} 
+          />
+        </div>
+        <div className='w-1/3 m-2'>
+          <h1 className='m-4 text-3xl border-b-2 text-center border-red-600 tracking-widest p-1'>{movie.title}</h1>
+          <p className='m-4 my-6 text-md'>{description}</p>
+        </div>
+      </div>
+      <div className='flex flex-row flex-wrap justify-center items-center m-4'>
+        {genresName.map(genre => (<p className='text-lg my-4 mx-2'>{genre}</p>))}
+      </div>
     </div>
-        {/* cast */}
-      <p className='m-3 text-md line-clamp-4'>{description}</p>
-  </>
   )
 }
