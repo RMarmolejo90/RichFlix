84bf463610f4bb643bbd6128ff883265b8c6a888
Richard Marmolejo <109112094+RMarmolejo90@users.noreply.github.com>
2024-02-23 02:31:32 +0000

    add details backdrop image

--- richflix/src/app/movies/[movieId]/page.tsx
+++ richflix/src/app/movies/[movieId]/page.tsx
@@ -13,28 +13,42 @@ export default async function page({ params: { movieId } }: Params) {
 
     const movie = await fetchMovieDetails(movieId);
     const posterSize: string =  "w780"; 
+    const backdropPath: string = movie.backdrop_path;
     const altTag: string =  movie.title ? movie.title : "Movie Poster Image";
     const description = movie?.overview??"";
     const genres = Object.values(movie.genres);
     const genresName = genres.map(genre => genre.name);
+    const backdrop: string = `https://image.tmdb.org/t/p/w1280${backdropPath}`;
 
   // fetch movie === movie.id from movieDB API
 
   // data === response.data
   // return data - destructure {image, cast, description}
   const posterUrl = `http://image.tmdb.org/t/p/${posterSize}${movie.poster_path}`;
-  console.log(genres);
-  console.log(genresName);
     return (
     <div className='flex flex-col justify-center items-center'>
+      <Image 
+        src={backdrop} 
+        alt={altTag} 
+        quality={100}
+        sizes="100vw"
+        fill
+        style={{
+          objectFit: 'cover',
+          objectPosition: '50% 10%',
+        }}
+        className='-z-10 brightness-[.25] grayscale '
+        priority={true}
+      />
       <div className='flex flex-col lg:flex-row items-center justify-center mt-16 w-full'>
         <div className='m-2'> 
           <Image 
             src={posterUrl}
             alt={altTag}
             width={200} 
-            height={200}
+            height={0}
             quality={100} 
+            className='h-auto'
           />
         </div>
         <div className='w-full lg:w-1/3 m-2'>
@@ -42,7 +56,7 @@ export default async function page({ params: { movieId } }: Params) {
           <p className='m-4 my-6 text-md leading-6'>{description}</p>
         </div>
       </div>
-      <div className='flex flex-row flex-wrap justify-center items-center m-4'>
+      <div className='flex flex-row flex-wrap justify-center items-center m-4 '>
         {genresName.map(genre => (<p className='text-lg my-4 mx-2'>{genre}</p>))}
       </div>
     </div>
