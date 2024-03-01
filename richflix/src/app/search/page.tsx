6b4a7f234091e71f248aab1b1ae17647cc189758
Richard Marmolejo <109112094+RMarmolejo90@users.noreply.github.com>
2024-03-01 21:25:10 +0000

    add styling to search page

--- richflix/src/app/search/page.tsx
+++ richflix/src/app/search/page.tsx
@@ -25,13 +25,13 @@ export default function search() {
   return (
     
     <div className='flex flex-col justify-center items-center w-full'>
-      <form className='text-slate-700 mt-40 mb-10 z-80' onSubmit={submitSearch}>
-        <input onChange={handleInput} className='m-4 p-2 tracking-wider' type="text" />
-        <button className='m-4 text-slate-300'  type="submit">Search</button>
+      <form className='text-slate-700 mt-40 mb-10 z-80 drop-shadow-[0_0_20px_rgb(220,38,38)]' onSubmit={submitSearch}>
+        <input onChange={handleInput} className='m-4 px-8 p-2 tracking-wider ' type="text" />
+        <button className='m-4 text-slate-300 drop-shadow-[0_0_10px_rgb(220,38,38)] font-semibold uppercase'  type="submit">Search</button>
       </form>
       <div className='flex flex-col group'>
       {searchResults && (
-        <div className='mb-48'>
+        <div className='mb-48 drop-shadow-[0_0_90px_rgb(220,38,38)]'>
           <div className='border-t-2 border-l-2 rounded-tl-3xl border-red-500'>
             <h2 className='p-2 pl-12 font-semibold text-xl tracking-wide'>{searchWords}</h2>
           </div>
