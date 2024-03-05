54cf6a680b50f02e6aa9247fe19ba47d462c8f97
Richard Marmolejo <109112094+RMarmolejo90@users.noreply.github.com>
2024-03-05 21:12:18 +0000

    bug fix - syntax error

--- richflix/src/app/dashboard/page.tsx
+++ richflix/src/app/dashboard/page.tsx
@@ -18,7 +18,7 @@ export default function Dashboard() {
       <div className='w-full drop-shadow-[0_0_90px_rgba(255,190,230,1)] flex flex-col justify-center items-center'>
         <div className='p-4 lg:w-2/3 flex flex-col justify-center items-center text-center'>
           <h2 className='p-10 text-5xl font-bold text-slate-600 uppercase'>Coming Soon</h2>
-          <h3 className='py-4 lg:p-10 text-2xl leading-loose font-semibold'>Thanks for visiting my website. I haven&apos;t implemented the user dashboard yet, but eventually this is where you&apos;ll find the movies you've saved to your watchlist. So Stay tuned for updates.</h3> 
+          <h3 className='py-4 lg:p-10 text-2xl leading-loose font-semibold'>Thanks for visiting my website. I haven&apos;t implemented the user dashboard yet, but eventually this is where you&apos;ll find the movies you&apos;ve saved to your watchlist. So Stay tuned for updates.</h3> 
           <h3 className='py-4 lg:p-10 text-4xl leading-loose font-semibold'>ENJOY!</h3>
         </div>
       </div>
